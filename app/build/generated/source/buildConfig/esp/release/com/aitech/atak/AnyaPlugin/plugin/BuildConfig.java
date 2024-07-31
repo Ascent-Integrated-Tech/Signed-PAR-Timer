4acec198b9bb48049dc074ec86c1eed2e29a609e
Tristin Sorrells <tristinsorrells1@gmail.com>
2024-07-31 15:17:02 -0500

    Remove .gitignore

---
+++ app/build/generated/source/buildConfig/esp/release/com/aitech/atak/AnyaPlugin/plugin/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aitech.atak.AnyaPlugin.plugin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.aitech.atak.AnyaPlugin.plugin.esp";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "esp";
+  public static final int VERSION_CODE = 1722456039;
+  public static final String VERSION_NAME = "0.8 (59a3ec1f) - [5.1.0]";
+  // Field from the variant API
+  public static final String ATAK_PACKAGE_NAME = "com.atakmap.app.civ";
+}
